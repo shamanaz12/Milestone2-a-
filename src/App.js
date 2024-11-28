75e92cff828569d89e8211a0fe3e8f392aa5f693
Shama Naz <shama20302022@gmail.com>
2024-11-28 05:02:38 -0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import './style.css';
 
+import React,{useState}from 'react'
 function App() {
+  const [data,setData]=useState("Governor House")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='h'>Hooks in React {data}</h1>
+      <button className='b'onClick={()=>setData("saturday slots: 2 to 5 pm")}>Update State</button>
     </div>
   );
 }
